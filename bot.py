de71322a47066df955512da0ad9a5ec11f74916a
vainnor <me@carsonberget.com>
2025-08-22 13:07:42 -0500

    more logging

--- bot.py
+++ bot.py
@@ -10,11 +10,15 @@ import logging.handlers
 from config import DISCORD_TOKEN
 from discord.ext import commands
 
+from errors.discord import ExtensionLoadError
+
 intents = discord.Intents.all()
 intents.messages = True
 intents.members = True
 
 bot = commands.Bot(command_prefix='!', intents=intents, case_insensitive=True)
+
+logger = logging.getLogger('__name__')
 
 def setup_logging():
     config_file = pathlib.Path("logging_conf.json")
@@ -33,24 +37,34 @@ async def on_command_error(ctx, error):
     elif isinstance(error, commands.MissingPermissions):
         await ctx.send("You don't have the necessary permissions to run this command.")
     else:
-        print(f"Unhandled command error in {ctx.command}: {error}")
+        logger.error(f"Unhandled command error in {ctx.command}: {error}", exc_info=True)
         await ctx.send(f"An unexpected error occurred: `{error}`")
 
 
 async def load_extensions():
     for filename in os.listdir("./extensions"):
         if filename.endswith(".py"):
+            extension_name = filename[:-3]
             try:
-                await bot.load_extension(f"extensions.{filename[:-3]}")
-                print(f"Loaded extension: {filename[:-3]}")
-            except Exception as e:
-                print(f"Failed to load extension {filename[:-3]}: {e}")
+                await bot.load_extension(f"extensions.{extension_name}")
+                logger.info(f"Loaded extension: {extension_name}")
+            except ExtensionLoadError as e:
+                logger.error(f"An unexpected error occurred while loading extension "
+                             f"'{extension_name}': {e}", exc_info=True)
 
 async def main():
     setup_logging()
+    logger.info("Bot starting...")
     await load_extensions()
-    await bot.start(DISCORD_TOKEN)
-    print("Bot Started")
+
+    try:
+        await bot.start(DISCORD_TOKEN)
+    except discord.LoginFailure:
+        logger.critical("Failed to log in to Discord. Please check your token.")
+    except Exception as e:
+        logger.critical("An unhandled exception occurred during bot startup/runtime: %s", e, exc_info=True)
+    finally:
+        logger.info("Bot shutting down.") # Log shutdown
 
 
 if __name__ == "__main__":
